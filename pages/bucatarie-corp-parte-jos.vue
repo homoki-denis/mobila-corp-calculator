a2ed2ace9e413701807c552b7aab8d6c2f17afbe
homoki-denis <homoki.denisth@gmail.com>
2024-02-09 13:46:11 +0200

    fix: margin and width on parte jos page

--- pages/bucatarie-corp-parte-jos.vue
+++ pages/bucatarie-corp-parte-jos.vue
@@ -1,59 +1,46 @@
 <template>
-  <div class="flex items-center justify-center flex-wrap min-h-90">
-    <form class="bg-white p-8 rounded shadow-md w-full max-w-md mx-4 mb-4">
-      <div class="mb-4 text-gray-700">
-        <label class="block text-sm font-bold mb-2" for="corp-parte-jos">
-          Bucatarie corp parte jos
-        </label>
-        <input
-          v-model="dimensiuneFund"
-          class="w-full px-3 py-2 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-          id="dimensiuneFund"
-          type="number"
-          placeholder="Dimensiune fund"
-        />
-      </div>
-      <button
-        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        @click="calculeazaDimensiuni"
+  <div class="w-full flex justify-center items-center min-h-90">
+    <div class="flex w-full items-center justify-center flex-wrap gap-2">
+      <form class="bg-white p-8 rounded shadow-md w-full max-w-md">
+        <div class="mb-4 text-gray-700">
+          <label class="block text-sm font-bold mb-2" for="corp-parte-jos">
+            Bucatarie corp parte jos
+          </label>
+          <input
+            v-model="dimensiuneFund"
+            class="w-full px-3 py-2 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+            id="dimensiuneFund"
+            type="number"
+            placeholder="Dimensiune fund"
+          />
+        </div>
+        <button
+          class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          @click="calculeazaDimensiuni"
+        >
+          Calculeaza
+        </button>
+      </form>
+
+      <div
+        v-for="rez in rezultate"
+        class="bg-white text-black px-8 py-12 rounded shadow-md w-full max-w-md"
       >
-        Calculeaza
-      </button>
-      <!-- <div v-if="rezultate" class="mt-4">
         <p class="text-gray-700">
           Dimensiuni laterale: lungime
-          {{ rezultate.dimensiuneLaterala.lungime }}, latime
-          {{ rezultate.dimensiuneLaterala.latime }}
+          {{ rez.dimensiuneLaterala.lungime }}, latime
+          {{ rez.dimensiuneLaterala.latime }}
           <br />
           Dimensiuni Piese de legătură: lungime
-          {{ rezultate.dimensiunePieseLegatura.lungime }}, latime
-          {{ rezultate.dimensiunePieseLegatura.latime }}
+          {{ rez.dimensiunePieseLegatura.lungime }}, latime
+          {{ rez.dimensiunePieseLegatura.latime }}
           <br />
-          Dimensiuni PFL: lungime {{ rezultate.dimensiunePFL.lungime }}, latime
-          {{ rezultate.dimensiunePFL.latime }}<br />
-          Dimensiuni Poliță: lungime {{ rezultate.dimensiunePolita.lungime }},
-          latime {{ rezultate.dimensiunePolita.latime }}
+          Dimensiuni PFL: lungime {{ rez.dimensiunePFL.lungime }}, latime
+          {{ rez.dimensiunePFL.latime }}<br />
+          Dimensiuni Poliță: lungime {{ rez.dimensiunePolita.lungime }}, latime
+          {{ rez.dimensiunePolita.latime }}
         </p>
-      </div> -->
-    </form>
-    <div
-      v-for="rez in rezultate"
-      class="bg-white text-black px-8 py-12 rounded shadow-md w-full max-w-md mx-4 mb-4"
-    >
-      <p class="text-gray-700">
-        Dimensiuni laterale: lungime
-        {{ rez.dimensiuneLaterala.lungime }}, latime
-        {{ rez.dimensiuneLaterala.latime }}
-        <br />
-        Dimensiuni Piese de legătură: lungime
-        {{ rez.dimensiunePieseLegatura.lungime }}, latime
-        {{ rez.dimensiunePieseLegatura.latime }}
-        <br />
-        Dimensiuni PFL: lungime {{ rez.dimensiunePFL.lungime }}, latime
-        {{ rez.dimensiunePFL.latime }}<br />
-        Dimensiuni Poliță: lungime {{ rez.dimensiunePolita.lungime }}, latime
-        {{ rez.dimensiunePolita.latime }}
-      </p>
+      </div>
     </div>
   </div>
 </template>
@@ -65,7 +52,6 @@ const dimensiuneFund = ref(0);
 const rezultate = ref([]);
 
 // Corp partea de jos
-
 const calculeazaDimensiuni = (e) => {
   e.preventDefault();
   const generareDimensiuni = (dimensiuneFund) => {
