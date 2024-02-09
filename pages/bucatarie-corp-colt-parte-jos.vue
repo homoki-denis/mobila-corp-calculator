90e6b10205aaeeb51b7f48088028dc7269a0b551
homoki-denis <homoki.denisth@gmail.com>
2024-02-09 17:28:55 +0200

    fix: corp colt parte jos

--- pages/bucatarie-corp-colt-parte-jos.vue
+++ pages/bucatarie-corp-colt-parte-jos.vue
@@ -4,9 +4,35 @@
       <div
         class="bg-white text-black px-8 py-8 rounded shadow-md w-full max-w-md"
       >
+        <p class="text-md font-bold mb-2">Bucatarie corp colt jos</p>
         <p v-for="rez in rezultate" class="text-gray-700">
           {{ rez.nume }}: lungime {{ rez.lungime }}, latime {{ rez.latime }}
         </p>
+        <div class="flex items-end">
+          <input
+            v-model="numarElemente"
+            type="number"
+            placeholder="Introdu numarul de elemente"
+            class="p-1.5 mt-2 border-2 rounded border-slate-950 hover:border-slate-500"
+          />
+          <button
+            class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 ml-2 rounded focus:outline-none focus:shadow-outline"
+            @click="afiseazaElemente"
+          >
+            Afiseaza
+          </button>
+        </div>
+      </div>
+      <div
+        v-for="array in arrayRezultate"
+        class="bg-white text-black px-8 py-8 rounded shadow-md w-full max-w-md"
+      >
+        <div v-for="afis in array">
+          <p>
+            {{ afis.nume }}: lungime {{ afis.lungime }}, latime
+            {{ afis.latime }}
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -15,9 +41,17 @@
 <script setup>
 import { ref } from "vue";
 
+const numarElemente = ref(0);
+const arrayRezultate = ref([]);
+
 const rezultate = ref([
   {
     nume: "Dimensiune Laterala",
+    lungime: 728,
+    latime: 500,
+  },
+  {
+    nume: "Dimensiune Laterala 2",
     lungime: 728,
     latime: 500,
   },
@@ -58,5 +92,12 @@ const rezultate = ref([
   },
 ]);
 
-console.log(rezultate.value.nume);
+const afiseazaElemente = () => {
+  arrayRezultate.value = [];
+
+  for (let i = 0; i < numarElemente.value; i++) {
+    arrayRezultate.value.push([...rezultate.value]);
+  }
+  console.log(arrayRezultate);
+};
 </script>
