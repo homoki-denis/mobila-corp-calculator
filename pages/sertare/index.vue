534d68690a3e935eaca606b03a9c985ebdcbb8fc
homoki-denis <homoki.denisth@gmail.com>
2024-02-22 12:56:03 +0200

    fix: added sertare haffele

--- pages/sertare/index.vue
+++ pages/sertare/index.vue
@@ -18,6 +18,14 @@
           Sertare blum
         </button>
       </NuxtLink>
+
+      <NuxtLink to="/">
+        <button
+          class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Sertare haffele
+        </button>
+      </NuxtLink>
     </div>
   </div>
 </template>
